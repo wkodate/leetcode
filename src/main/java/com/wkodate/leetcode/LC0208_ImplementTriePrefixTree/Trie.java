ada3c118236cc6cc605fcc466d2b4bc43cb02201
wkodate <hbwandeow@gmail.com>
2020-05-15 02:27:59 +0900

    day14 - May LeetCoding Challenge

---
+++ src/main/java/com/wkodate/leetcode/LC0208_ImplementTriePrefixTree/Trie.java
@@ -0,0 +1,100 @@
+package com.wkodate.leetcode.LC0208_ImplementTriePrefixTree;
+
+public class Trie {
+
+    public class TrieNode {
+
+        private TrieNode[] links;
+
+        private final int R = 26;
+
+        private boolean isEnd;
+
+        public TrieNode() {
+            links = new TrieNode[R];
+        }
+
+        public boolean containsKey(char ch) {
+            return links[ch - 'a'] != null;
+        }
+
+        public TrieNode get(char ch) {
+            return links[ch - 'a'];
+        }
+
+        public void put(char ch, TrieNode node) {
+            links[ch - 'a'] = node;
+        }
+
+        public void setEnd() {
+            isEnd = true;
+        }
+
+        public boolean isEnd() {
+            return isEnd;
+        }
+
+    }
+
+    private TrieNode root;
+
+    /** Initialize your data structure here. */
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char currentChar = word.charAt(i);
+            if (!node.containsKey(currentChar)) {
+                node.put(currentChar, new TrieNode());
+            }
+            node = node.get(currentChar);
+        }
+        node.setEnd();
+    }
+
+    /** search a prefix or whole key in trie and */
+    /** returns the node where search ends */
+    private TrieNode searchPrefix(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char curLetter = word.charAt(i);
+            if (!node.containsKey(curLetter)) {
+                return null;
+            }
+            node = node.get(curLetter);
+        }
+        return node;
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode node = searchPrefix(word);
+        return node != null && node.isEnd();
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TrieNode node = searchPrefix(prefix);
+        return node != null;
+    }
+
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+
+        trie.insert("apple");
+        // true
+        System.out.println(trie.search("apple"));
+        // false
+        System.out.println(trie.search("app"));
+        // true
+        System.out.println(trie.startsWith("app"));
+        trie.insert("app");
+        // true
+        System.out.println(trie.search("app"));
+    }
+
+}
