684c8695551eee021710233c6bfdfdd8ca585c6b
wkodate <hbwandeow@gmail.com>
2021-03-13 13:55:09 +0900

    solved #322

--- src/main/java/com/wkodate/leetcode/LC0322_CoinChange/Solution.java
+++ src/main/java/com/wkodate/leetcode/LC0322_CoinChange/Solution.java
@@ -1,37 +1,25 @@
 package com.wkodate.leetcode.LC0322_CoinChange;
 
+import java.util.Arrays;
+
 public class Solution {
 
-    private int coinChange(int[] coins, int rem, int[] count) {
-        if (rem < 0) {
-            return -1;
-        }
-        if (rem == 0) {
-            return 0;
-        }
-        if (count[rem - 1] != 0) {
-            return count[rem - 1];
-        }
-        int min = Integer.MAX_VALUE;
-        for (int coin : coins) {
-            int res = coinChange(coins, rem - coin, count);
-            if (res >= 0 && res < min) {
-                min = 1 + res;
+    public int coinChange(int[] coins, int amount) {
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, max);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (coins[j] <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+                }
             }
         }
-        if (min == Integer.MAX_VALUE) {
-            count[rem - 1] = -1;
-        } else {
-            count[rem - 1] = min;
+        if (dp[amount] > amount) {
+            return -1;
         }
-        return count[rem - 1];
-    }
-
-    public int coinChange(int[] coins, int amount) {
-        if (amount < 1) {
-            return 0;
-        }
-        return coinChange(coins, amount, new int[amount]);
+        return dp[amount];
     }
 
     public static void main(String[] args) {
